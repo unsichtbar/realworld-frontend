70036017c0351898e81554c543383179cd1c1bb9
Alex Lindemann <exmann94@gmail.com>
2020-09-25 01:20:27 -0500

    landing page

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,5 +1,24 @@
 import * as React from "react";
+import { useQuery } from "react-query";
+import { ArticleDisplay } from "../article/view/Article";
+import { getGlobalFeed } from "./landing-api";
 
 export const Landing: React.FC<{}> = (props) => {
-  return <div>Home Page wowee</div>;
+  const articles = useQuery("globalFeed", getGlobalFeed);
+  return (
+    <>
+      <section>App Name</section>
+      <section>
+        <section>
+          <div>Global Feed</div>
+          <div>
+            {articles.data?.map((article) => (
+              <ArticleDisplay key={article.slug} article={article} />
+            ))}
+          </div>
+        </section>
+        <section>Popular Tags:</section>
+      </section>
+    </>
+  );
 };
